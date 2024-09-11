c80852fa82244e2871680062416dafe8684b3726
Federico Martin <FM311721@fi365.ort.edu.uy>
2024-09-10 21:19:33 -0300

    fix:Cambio de hash

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -2,10 +2,36 @@
 #include <string>
 #include <iostream>
 #include <limits>
-
+#include "tads/avl.cpp"
 using namespace std;
 
 int main()
 {
+    int N;
+    cin >> N;
+    AVL<int> * arbol= new AVL<int>;
+    for (int i=0; i<N; i++){
+        string p1;
+        cin >> p1;
+        if(p1=="ADD"){
+            int id=0;
+            string p3;
+            cin >> id >> p3;
+            arbol->add(id,p3);
+        }else if(p1=="FIND"){
+            int id=0;
+            cin >> id;
+            cout << arbol->find(id) << endl;
+        }else if(p1=="TOGGLE"){
+            int id;
+            cin >> id;
+            bool existe= arbol->toggle(id);
+            if(!existe){
+                cout << "libro_no_encontrado" << endl;
+            }
+        }else{
+            cout << arbol->getCount() << endl;
+        }
+    }
     return 0;
 }
--- ejercicio2.cpp
+++ ejercicio2.cpp
@@ -2,11 +2,37 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "tads/HashCerrado.cpp"
 
 using namespace std;
 
 int main()
 {
-    // TODO
+    int N;
+    cin >> N;
+    HashCerrado * tabla= new HashCerrado(N);
+    for (int i=0; i<N; i++){
+        string p1;
+        cin >> p1;
+        if(p1=="ADD"){
+            int id=0;
+            string p3;
+            cin >> id >> p3;
+            tabla->add(id,p3);
+        }else if(p1=="FIND"){
+            int id=0;
+            cin >> id;
+            cout << tabla->find(id) << endl;
+        }else if(p1=="TOGGLE"){
+            int id;
+            cin >> id;
+            bool existe= tabla->toggle(id);
+            if(!existe){
+                cout << "libro_no_encontrado" << endl;
+            }
+        }else{
+            cout << tabla->getCount() << endl;
+        }
+    }
     return 0;
 }
