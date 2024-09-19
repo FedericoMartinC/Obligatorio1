f03d7f7d1d34a19aac7a79d52acea91da0ff3edb
Federico Martin <FM311721@fi365.ort.edu.uy>
2024-09-19 15:38:23 -0300

    Ejercicio 3

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -2,11 +2,34 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "tads/MinHeap.cpp"
+#include "tads/HashAbierto.cpp"
 
 using namespace std;
 
 int main()
 {
-    // TODO
+    int N;
+    cin >> N;
+    bool* salio= new bool[N+1]();
+    MinHeap * obj= new MinHeap(N);
+    for(int i=0;i<N;i++){
+        int id;
+        int precio;
+        cin >> id >> precio;
+        obj->insertar(id,precio);
+    }
+    int k;
+    cin >> k;
+    int a=0;
+    while(a!=k){
+        int id=obj->tope();
+        if(!salio[id]){
+            cout << obj->tope() << endl;
+            salio[id]=true;
+            a++;
+        }
+        obj->eliminar(1);
+    }
     return 0;
 }
--- tads/MinHeap.cpp
+++ tads/MinHeap.cpp
@@ -8,8 +8,107 @@ using namespace std;
 class MinHeap
 {
 private:
-    
+    struct datoDoble {
+        int id;
+        int precio;
+        datoDoble(int unId, int unPrecio): id(unId), precio(unPrecio){}
+    };
+    int capacidad;
+    int primerLibre;
+    datoDoble** arr;
+
+    void cambio(int pos1, int pos2){
+        datoDoble* aux = arr[pos1];
+        arr[pos1]=arr[pos2];
+        arr[pos2]=aux;
+    }
+
+    int padre(int i){
+        return i / 2;
+    }
+
+    int hIzq(int i){
+        return 2 * i;
+    }
+
+    int hDer(int i){
+        return (2 * i ) + 1;
+    }
+
+    void subir(int pos){
+        if(pos == 1){
+            return;
+        }else{
+            int posPadre= padre(pos);
+            if(arr[posPadre]->precio > arr[pos]->precio){
+                cambio(pos,posPadre);
+                subir(posPadre);
+            }else if(arr[posPadre]->precio == arr[pos]->precio && arr[posPadre]->id < arr[pos]->id){
+                cambio(pos,posPadre);
+                subir(posPadre);
+            }
+        }
+    }
+
+    void bajar (int pos){
+        int posIzq= hIzq(pos);
+        int posDer= hDer(pos);
+        if(posIzq < primerLibre){
+            int posMenor= posIzq;
+            if(posDer < primerLibre){
+                if(arr[posIzq]->precio > arr[posDer]->precio){
+                    posMenor= posDer;
+                }else if(arr[posIzq]->precio == arr[posDer]->precio && arr[posIzq]->id < arr[posDer]->id){
+                    posMenor= posDer;
+                }
+            }
+
+            if(arr[pos]->precio > arr[posMenor]->precio){
+                cambio(pos, posMenor);
+                bajar(posMenor);
+            }else if(arr[pos]->precio == arr[posMenor]->precio && arr[pos]->id < arr[posMenor]->id){
+                cambio(pos, posMenor);
+                bajar(posMenor);
+            }   
+
+        }
+    }
+
+
 public:
-    
+    MinHeap(int _capacidad) {
+        capacidad = _capacidad;
+        primerLibre = 1;
+        arr= new datoDoble*[capacidad+1]();
+        for (int i=0; i <= capacidad; i++){
+            arr[i]=NULL;
+        }
+    }
+
+    void insertar(int id, int precio){
+        datoDoble * nuevo= new datoDoble(id,precio);
+        arr[primerLibre]=nuevo;
+        primerLibre++;
+        subir(primerLibre-1);
+    }
+
+    int tope(){
+        return arr[1]->id;
+    }
+
+    void eliminar(int pos){
+        cambio(pos,primerLibre-1);
+        primerLibre--;
+        subir(pos);
+        bajar(pos);
+    }
+
+    void cambiarPrecio(int pos, int precio){
+        arr[pos]->precio=precio;
+        subir(pos);
+    }
+
+
+
 };
 #endif
